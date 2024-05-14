985906acdd778671e9eded67299946668ac936d2
omarjafor <omarjaforchy@gmail.com>
2024-05-14 22:49:40 +0600

    send input message

--- frontend/src/components/SendInput.jsx
+++ frontend/src/components/SendInput.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import { IoSend } from "react-icons/io5";
 
 const SendInput = () => {
+    const [message, setMessage] = useState('');
+    const submitHandler = (e) => {
+        e.preventDefault();
+        alert(message);
+    }
     return (
-        <form action="" className="px-4 my-3">
+        <form onSubmit={submitHandler} action="" className="px-4 my-3">
             <div className="w-full relative">
-                <input type="text" name="" id="" placeholder="Send a message...." className="border text-sm rounded-lg block w-full p-3 border-zinc-500 bg-gray-600 text-white" />
+                <input
+                value={message}
+                onChange={(e)=>setMessage(e.target.value)} type="text" name="" id="" placeholder="Send a message...." className="border text-sm rounded-lg block w-full p-3 border-zinc-500 bg-gray-600 text-white" />
                 <button className="absolute flex inset-y-0 end-0 items-center pr-4">
                     <IoSend />
                 </button>
