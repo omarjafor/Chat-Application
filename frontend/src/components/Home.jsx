40eed09aa3e4cd8e20312664f87c7affa021c2c8
omarjafor <omarjaforchy@gmail.com>
2024-05-22 23:44:56 +0600

    some change

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,12 +2,9 @@ import { useEffect } from "react";
 import MessageBox from "./MessageBox";
 import Sidebar from "./Sidebar";
 
-
 const Home = () => {
-    
-    useEffect( () => {
 
-    } , [])
+    
 
     return (
         <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
